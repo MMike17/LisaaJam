310cf9744586941749e19729b8e831034e6ca8ac
Mike <matthews.r.mike@gmail.com>
2022-02-20 14:32:45 +0100

    Fix AI kill

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -42,6 +42,8 @@ public partial class EnemyBase : MonoBehaviour
 
 		foreach (Collider collider in colliders)
 			collider.enabled = false;
+
+		gameObject.SetActive(false);
 		// }, /*animation duration*/);
 	}
 
